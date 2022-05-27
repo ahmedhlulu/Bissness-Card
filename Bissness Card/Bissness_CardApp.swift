3a0e418dec0361f9c0944c3e4cd64b94e9805ee3
ahmedhlulu <100189769+ahmedhlulu@users.noreply.github.com>
2022-05-27 19:02:45 +0300

    Initial Commit

---
+++ Bissness Card/Bissness_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bissness_CardApp.swift
+//  Bissness Card
+//
+//  Created by Ahmed on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct Bissness_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
